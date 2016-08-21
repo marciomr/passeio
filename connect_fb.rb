f3a02855cefc4c802e9abeeddbad74df72dae3d4
Marcio M. Ribeiro <marcio.mr@gmail.com>
2016-08-21 01:24:25 -0300

    mudança considerável no código do connect_fb e inserção da possibilidade de reconectar com diferentes usuários para evitar erros.

--- connect_fb.rb
+++ connect_fb.rb
@@ -1,113 +1,139 @@
-#!/usr/bin/env ruby
-# encoding: utf-8
-
+# coding: utf-8
 require 'koala'
 require 'mechanize'
 require 'highline/import'
 require 'yaml'
 
 class FB
-    # Configuracao global?
-    REDIRECT_URL = "http://gpopai.usp.br/"
+  attr_reader :user, :users
+  
+  def initialize
+    load_config # le as configuracoes do app
+    # conecta com a Graph API
+    puts "Conectando..."
+    @oauth = Koala::Facebook::OAuth.new(@config['app_id'], @config['app_secret'], @config['url'])
+    load_users # le o arquivo dos usuarios
+    @user = @users.last
+    puts "Conectado como #{@user['email']}"
+    @graph = Koala::Facebook::API.new(@user['token'])
+  end
 
-    # Proxima pagina de resultados paginados
-    def next_page(klass)
-        begin
-            return klass.next_page if klass
-        rescue
-            sleep 3
-            return next_page klass
-        end
+  # reconecta com um usuario aleatorio
+  def reconnect
+    @user = @users.sample
+    @graph = Koala::Facebook::API.new(@user['token'])
+  end
+  
+  # o fb pagina alguns resultados
+  # este metodo aplica o bloco &block em todas as paginas
+  def total(klass, &block)
+    loop do
+      break if(!klass)
+      klass.each(&block)
+      klass = next_page klass
     end
-    # o fb pagina alguns resultados
-    # este metodo aplica o bloco &block em todas as paginas
-    def total(klass, &block)
-        loop do
-            break if(!klass)
-            klass.each(&block)
-            klass = next_page klass
-        end
-    end
-    def get_config
-        unless File.exist? '.config'
-            File.open('.config','w') do |h|
-                config = {}
-                config['email'] = ask "Digite seu email"
-                config['pswd'] = ask("Digite sua senha do Facebook") { |q| q.echo = '*' }
-                config['app_id'] = ask "Digite seu app id"
-                config['app_secret'] = ask "Digite seu app secret"
-                h.write config.to_yaml
-            end
-        end
-        @config = YAML.load_file(".config")
-    end
-    # se a conexão falhar espere 5 segundos e tente de novo
-    def connect(id, type, args = {})
-        begin
-            if type == "self"
-                return @graph.get_object(id)
-            else
-                return @graph.get_connections(id, type, args)
-            end
-        rescue Exception => e
-          #puts "Retrying failed connection..."
-          puts e.message
-          #sleep 3
-          #return connect(id, type)
-        end
-    end
-#    def graph
-#        @graph
-#    end
-    def total_likes(id)
-        return @graph.get_object(id, fields: "likes.summary(true)")['likes']['summary']['total_count']
-    end
-    def total_attending(id)
-        return @graph.get_object(id, fields: "attending.summary(true)")['attending']['summary']['count']
-    end
+  end
 
-    def get_page(id)
-      begin
-        return @graph.get_object(id, fields: ["engagement", "name", "insights", "category"])
-      rescue Exception => e
-#        puts "Retrying failed connection..."
-        puts e.message
- #       sleep 3
- #       return get_page(id)
+  def connect(id, type, args = {})
+    begin
+      if type == "self"
+        return @graph.get_object(id, args)
+      else
+        return @graph.get_connections(id, type, args)
+      end
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace
+    end
+  end
+
+  def total_likes(id)
+    pg = connect(id, "self", fields: "likes.summary(true)")
+    begin
+      return pg['likes']['summary']['total_count']
+    rescue Exception => e
+      puts e.message
+    end
+  end
+
+  def get_page(id)
+    connect(id, "self", fields: ["engagement", "name", "category"])
+  end
+
+  def brazilian?(id)
+    array = connect(id, "insights/page_fans_country")
+    paises = array[0]["values"][0]["value"]
+    return paises["BR"] == paises.values.max
+  end
+
+  # cria um usuario e guarda no arquivo .users
+  def new_user
+    users = File.zero?('.users') ? [] : YAML.load_file('.users')
+    user = {}
+    user['email'] = ask "Digite seu email"
+    pswd = ask("Digite sua senha do Facebook") { |q| q.echo = '*' }
+
+    begin
+      code = get_access_code(@oauth.url_for_oauth_code, user['email'], pswd)
+    rescue Koala::Facebook::APIError => e
+      puts "Houve um erro ao resgatar o token de autorização. Copie e cole o seguinte URL"
+      puts "em um browser, autorize o app e tente novamente"
+      puts @oauth.url_for_oauth_code
+      exit
+    end
+    user['token'] = @oauth.get_access_token(code)
+    users << user
+    File.open('.users','w') do |f|
+      f.write users.to_yaml
+    end
+  end
+  
+  private
+  # pega ou cria um arquivo de configuracao
+  def load_config
+    unless File.exist? '.config'
+      File.open('.config','w') do |f|
+        config = {}
+        config['url'] = ask "Digite o URL do app"
+        # "http://gpopai.usp.br/"
+        config['app_id'] = ask "Digite seu app id"
+        config['app_secret'] = ask "Digite seu app secret"
+        f.write config.to_yaml
       end
     end
-    # entra na pagina, loga no FB e devolve o código
-    def get_access_code(page)
-        @agent = Mechanize.new
-        @agent.redirect_ok = :all
-        @agent.follow_meta_refresh = :anywhere
+    @config = YAML.load_file(".config")
+  end
 
-        page = @agent.get page
-        form = page.form_with(method: "POST")
-        form.email = @config['email']
-        form.pass = @config['pswd']
-        @agent.submit form
-        uri = URI(@agent.page.uri)
-        CGI.parse(uri.query)['code'].first
+  def get_access_code(page, email, pswd)
+    @agent = Mechanize.new
+    @agent.redirect_ok = :all
+    @agent.follow_meta_refresh = :anywhere
+
+    page = @agent.get page
+    form = page.form_with(method: "POST")
+    form.email = email
+    form.pass = pswd
+    @agent.submit form
+    uri = URI(@agent.page.uri)
+    CGI.parse(uri.query)['code'].first
+  end
+
+  # le o arquivo de usuarios e guarda em um atributo
+  # se o arquivo estiver vazio cria um usuario
+  def load_users
+    new_user if File.zero?('.users')
+    @users = YAML.load_file('.users')
+  end
+
+  # Proxima pagina de resultados paginados
+  def next_page(klass)
+    begin
+      return klass.next_page if klass
+    rescue
+      sleep 3
+      return next_page klass
     end
-    def initialize
-        get_config # le as configuracoes de usuario/senha
-
-        puts "Conectando..."
-
-        # conecta com a Graph API
-        @oauth = Koala::Facebook::OAuth.new(@config['app_id'], @config['app_secret'], REDIRECT_URL)
-        begin
-            code = get_access_code(@oauth.url_for_oauth_code)
-        rescue
-            puts "Houve um erro ao resgatar o token de autorização. Copie e cole o seguinte URL"
-            puts "em um browser, autorize o app e tente novamente"
-            puts @oauth.url_for_oauth_code
-            exit
-        end
-        token = @oauth.get_access_token(code)
-        @graph = Koala::Facebook::API.new(token)
-    end
+  end
 end
 
 Facebook = FB.new
--- run.rb
+++ run.rb
@@ -3,10 +3,8 @@ require './connect_fb.rb'
 require './modules.rb'
 require 'progress_bar'
 
-#TODO Paralelizar
-
-categories = [
-#  "Education",
+RAND_CONST = 30
+CATEGORIES = [
   "Media/News/Publishing",
   "News/Media Website",
   "Society/Culture Website",
@@ -18,17 +16,24 @@ categories = [
   "Non-Profit Organization",
   "Community Organization",
   "Non-Governmental Organization (NGO)",
-#  "Education Website",
   "Community",
   "Political Party",
   "Politician",
   "Journalist",
-#  "Author",
-#  "Teacher",
-#  "Writer",
   "Cause"
+  #  "Education",
+  #  "Author",
+  #  "Teacher",
+  #  "Writer",
+  #  "Education Website",
 ]
 
+if ARGV.size >= 1 && ARGV[0] == "-u"
+  Facebook.new_user
+  exit
+end
+
+# primeira página é arbitrária e precisa ser dada pelo usuário
 unless Page.exists?
   print "Digite o id de uma página para começar: "
   pg = Facebook.get_page(gets.chomp)
@@ -40,35 +45,53 @@ unless Page.exists?
 end
 
 loop do
+  # pega a pg com mais likes dentre as que ainda não foram visitadas
   from = Page.order(likes_count: :desc).where(category: categories).where(visited: false).limit(1).first
   from.visited = true
   from.save
 
+  # imprime algumas estatisticas de progresso
   puts "#{from.name} - #{from.likes_count} (#{from.category})"
   nodes = Page.where(category: categories).count
   visited = Page.where(visited: true).count
   percent = (100.0*visited/nodes).round 2
   puts "#{nodes} nós (#{visited} visitados #{percent}%) e #{Like.count} arestas (#{(Like.count/visited.to_f).round 2})"
+
+  # pega todos os likes de paginas brasileiras
   likes = Facebook.connect(from.fb_id, "likes", {limit: 300})
-  size = likes.size == 0 ? 1 : likes.size
+  next if likes.size == 0
   bar = ProgressBar.new size  
   Facebook.total(likes) do |like|
     bar.increment!
-    pg = Facebook.get_page(like['id'])
+    id = like['id']
+    pg = Facebook.get_page(id)
 
     begin
-      paises = pg['insights']['data'][0]["values"][0]["value"]
-      next if paises["BR"] < paises.values.max
-    rescue
+      # pula se não for brasileira
+      next unless Facebook.brazilian?(id)
+      to = (Page.find_by_fb_id(id) or
+            Page.create(fb_id: id,
+                        name: pg['name'],
+                        likes_count: pg["engagement"]["count"],
+                        category: pg['category'],
+                        visited: false))
+      Like.create(from: from, to: to)
+    rescue Koala::Facebook::APIError => e
+      puts e.message
+      puts e.error_type
+      # se der um erro de limite de requisições, reconecta
+      next
+    rescue Exception => e
+      puts e.message
       next
     end
-
-    to = (Page.find_by_fb_id(like['id']) or
-          Page.create(fb_id: like['id'],
-                      name: pg['name'],
-                      likes_count: pg["engagement"]["count"],
-                      category: pg['category'],
-                      visited: false))
-    Like.create(from: from, to: to)
   end
+
+  # sorteia um usuário novo de tempos em tempos
+  if Random.rand(RAND_CONST) == 0
+    puts "Reconectando..."
+    Facebook.reconnect
+    puts "Conectado como #{Facebook.user['email']}"
+  end
+
 end
